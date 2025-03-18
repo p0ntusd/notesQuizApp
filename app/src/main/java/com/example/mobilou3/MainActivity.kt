9706bda85ff85c4dd75337a8b7f36c2162d741ce
dv23pdt <dv23pdt@cs.umu.se>
2025-03-18 10:22:29 +0100

    Main = GUI

--- app/src/main/java/com/example/mobilou3/MainActivity.kt
+++ app/src/main/java/com/example/mobilou3/MainActivity.kt
@@ -7,14 +7,55 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //ATTRIBUT
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //KOD
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    /**
+     * Saves the game state upon closing out
+     * of the application.
+     *
+     * @param   outState    Bundle to save the games state.
+     */
+    @Override
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+    }
+
+    /**
+     * Restores the previous game state upon reopening of
+     * the application.
+     */
+    @Override
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+    }
+
+    /**
+     * In case the game is ever paused. Im not
+     * sure if this is even nessecary, but
+     * there was some problems with controller/view
+     * connection so i will keep it just in case.
+     */
+    @Override
+    override fun onResume() {
+        super.onResume()
+    }
+
+
 }
